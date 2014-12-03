1542f3c0493cd8adea39761311f7fca738ae08e3
Zordius Chen <zordius@yahoo-inc.com>
2014-12-03 14:19:19 +0800

    more tests

---
+++ test/test-fluxexapp.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var assert = require('chai').assert,
+    app = require('./testApp');
+
+describe('a fluxex app', function () {
+    it('can be constructed by undefined', function (done) {
+        new app();
+        done();
+    });
+
+    it('can be constructed by number', function (done) {
+        var F = new app(123);
+        done();
+    });
+
+    it('can be constructed by object', function (done) {
+        var F = new app({stores: {sampleStore: {a: 1}}});
+
+        assert(1, F.getStore('sampleStore').get('a'));
+        done();
+    });
+});
--- test/test-serialize.js
+++ test/test-serialize.js
@@ -3,24 +3,6 @@
 var assert = require('chai').assert,
     app = require('./testApp');
 
-describe('a fluxex app', function () {
-    it('can be constructed by undefined', function (done) {
-        new app();
-        done();
-    });
-
-    it('can be constructed by number', function (done) {
-        var F = new app(123);
-        done();
-    });
-
-    it('can be constructed by object', function (done) {
-        var F = new app({stores: {sampleStore: {a: 1}}});
-        F.stores
-        done();
-    });
-});
-
 describe('app serialization', function () {
     it('can be constructed by undefined', function (done) {
         done();
