016e82e2059b14de5a2749c4aa7664abe65f5cdc
Zordius Chen <zordius@yahoo-inc.com>
2014-12-03 13:38:07 +0800

    travis and codeclimate

---
+++ travis.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "DEBUG ENV: ${TRAVIS_JOB_NUMBER} ${TRAVIS_BUILD_NUMBER} ..."
+
+if [ "${TRAVIS_BUILD_NUMBER}.2" != "${TRAVIS_JOB_NUMBER}" ]; then
+  echo "Only run sauce labs 1 time 1 commit... quit."
+  exit 0
+fi
+
+if [ "${TRAVIS_REPO_SLUG}" != "zordius/subtask.js" ]; then
+  echo "Skip deploy because this is a fork... quit."
+  exit 0
+fi
+
+# push coverage to codeclimate
+npm install codeclimate-test-reporter
+npm run-script coverage
+node_modules/.bin/codeclimate < coverage/lcov.info
