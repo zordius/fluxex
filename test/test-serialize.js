246958d4be115f369635030e50d5d06339069a3f
Zordius Chen <zordius@yahoo-inc.com>
2014-12-04 15:19:52 +0800

    add more test for serialize

--- test/test-serialize.js
+++ test/test-serialize.js
@@ -4,7 +4,18 @@ var assert = require('chai').assert,
     app = require('./testApp');
 
 describe('app serialization', function () {
-    it('can be constructed by undefined', function (done) {
+    it('store status will be updated in app', function (done) {
+        var App = new app();
+
+        App.getStore('sampleStore').set('a', 9, true);
+        assert.equal('{"stores":{"sampleStore":{"a":9}}}', App.toString());
+        done();
+    });
+
+    it('store can be restored in app', function (done) {
+        var App = new app({"stores":{"sampleStore":{"a":99}}});
+
+        assert.equal(99, App.getStore('sampleStore').get('a'));
         done();
     });
 });
