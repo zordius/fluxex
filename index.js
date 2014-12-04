66efd2a67e01d39f220b73585f158a712f3ede37
Zordius Chen <zordius@yahoo-inc.com>
2014-12-04 13:14:28 +0800

    move static functions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-module.exports = require('./lib/fluxex');
+var objectAssign = require('object-assign'),
+    Fluxex = require('./lib/fluxex');
+
+Fluxex.createApp = function (stores, prototype) {
+    var App = function FluxexApp() {
+        this.stores = stores;
+        Fluxex.apply(this, arguments);
+    };
+
+    if (!stores) {
+        throw new Error('You should create app with information of stores');
+    }
+
+    App.prototype = new Fluxex();
+    objectAssign(App.prototype, prototype);
+    App.prototype.constructor = App;
+
+    return App;
+};
+
+module.exports = Fluxex;
