38582f92dfaa3a358fdd1b952be75c821021d5f2
Zordius Chen <zordius@yahoo-inc.com>
2014-12-09 16:32:04 +0800

    refine action

--- examples/03-service/actions/api.js
+++ examples/03-service/actions/api.js
@@ -5,14 +5,19 @@ var yql = require('../services/yql');
 module.exports = {
     search: function (payload) {
         var p = payload.p * 1 || 0,
-            start = p * 10;
+            start = p * 10,
+            self = this;
 
         if (!payload.q) {
             return this.resovePromise({});
         }
 
         return yql('SELECT * FROM search.ec (' + start + ', 10) WHERE keyword="' + payload.q + '" and property="shopping"').then(function (O) {
-            console.log(O);
+            return {
+                total: O.result.total,
+                offset: start,
+                hits: O.result.hits
+            }
         });
     }
 };
