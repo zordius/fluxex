abca2c62ab79f121261c517d4e27c7c5805733e1
Zordius Chen <zordius@yahoo-inc.com>
2014-12-15 19:10:46 +0800

    try to add jsdoc

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
-
+/**
+ * Fluxex core module to provide .createApp() and mixin for React components.
+ * @module fluxex
+ */
 var objectAssign = require('object-assign'),
     Fluxex = require('./lib/fluxex');
 
+/**
+ * Create an fluxex application by provided defintion.
+ * @static
+ * @param {Object} Store defination as {storeName: implement} pairs
+ * @param {Object} The Html element defined as a React component
+ * @param {Object} Extra methods/properties want to be merged into the prototype of the flucex application
+ * @returns {Object} The created fluxex application instance.
+ */
 Fluxex.createApp = function (stores, HtmlJsx, prototype) {
     var App = function FluxexApp() {
         this.stores = stores;
