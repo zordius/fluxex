2c369d8ffc4b303fc59f25a2452ea37aae0ee0f0
Zordius Chen <zordius@yahoo-inc.com>
2014-12-16 12:10:14 +0800

    fix starter

--- starter.sh
+++ starter.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-npm install fluxex react node-jsx express browserify watchify reactify uglifyify jshint-stylish nodemon browser-sync gulp gulp-jshint gulp-react gulp-cached gulp-util vinyl-source-stream
+npm install --save react node-jsx express
+npm install --save-dev browserify watchify reactify uglifyify jshint-stylish nodemon browser-sync gulp gulp-jshint gulp-react gulp-cached gulp-util vinyl-source-stream
 mkdir actions
 mkdir components
 mkdir stores
