7ca55b77846997aa79021c2b14afe51f9c6e1616
Zordius Chen <zordius@yahoo-inc.com>
2014-12-18 12:42:25 +0800

    we still need browser.get when we have baseUrl

--- test_browser/spec.js
+++ test_browser/spec.js
@@ -1,15 +1,18 @@
 describe('React server side rendering', function () {
     it('should be good', function () {
+        browser.get('');
         expect(element.all(by.css('ul li')).count()).toEqual(3);
     });
 
     it('sold should be 0', function () {
+        browser.get('');
         expect(element(by.css('ul')).getInnerhtml()).toMatch(/Sold: 0/);
     });
 });
 
 describe('React client side binding', function () {
     it('should handle click', function () {
+        browser.get('');
         element(by.css('ul')).click().then(function () {
             expect(element(by.css('ul')).getInnerhtml()).toMatch(/Sold: 1/);
         });
