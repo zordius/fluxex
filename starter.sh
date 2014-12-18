91b45861cd959e618e143362e502e625ddbd932f
Zordius Chen <zordius@yahoo-inc.com>
2014-12-18 16:57:31 +0800

    add comments

--- starter.sh
+++ starter.sh
@@ -12,3 +12,22 @@ cp $BASE/server.js index.js
 cp $BASE/fluxexapp.js fluxexapp.js
 cp $BASE/gulpfile.js gulpfile.js
 cp $BASE/.jshintrc .
+
+echo '==================== THE STARTER PROJECT IS READY ===================='
+echo '= + index.js                    Your Server, add api URL here        ='
+echo '= + fluxexapp.js                Your APP, add store defination here  ='
+echo '= |                                                                  ='
+echo '= + actions/ +                  Add your actions here                ='
+echo '= |          + routing.js       Add routes here                      ='
+echo '= |          + page.js          Add page actions here                ='
+echo '= |          + api.js           Add api actions here                 ='
+echo '= |                                                                  ='
+echo '= + stores/                     Add your store here                  ='
+echo '= |                                                                  ='
+echo '= + components/ +               Add your components here             ='
+echo '= |             + Html.jsx      Use your components here             ='
+echo '======================================================================'
+echo ' * Run this to start the server:'
+echo
+echo '       node_modules/.bin/gulp develop'
+echo
