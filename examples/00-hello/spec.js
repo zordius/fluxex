ddab3f95ac96d01ab85549861a31482bba86e5c6
Zordius Chen <zordius@yahoo-inc.com>
2014-12-19 16:35:32 +0800

    fix spec

--- examples/00-hello/spec.js
+++ examples/00-hello/spec.js
@@ -1,18 +1,18 @@
 describe('React server side rendering', function () {
     it('should be good', function () {
-        browser.get('');
+        browser.get('test');
         expect(element.all(by.css('ul li')).count()).toEqual(3);
     });
 
     it('sold should be 0', function () {
-        browser.get('');
+        browser.get('test');
         expect(element(by.css('ul')).getInnerHtml()).toMatch(/Sold: 0/);
     });
 });
 
 describe('React client side binding', function () {
     it('should handle click', function () {
-        browser.get('');
+        browser.get('test');
         element(by.css('ul')).click().then(function () {
             expect(element(by.css('ul')).getInnerHtml()).toMatch(/Sold: 1/);
         });
