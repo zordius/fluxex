d976954f0ae53da761d0f1c1ec1446aca2f83f1c
Zordius Chen <zordius@yahoo-inc.com>
2014-12-19 17:13:27 +0800

    fix spec

--- examples/01-history-api/spec.js
+++ examples/01-history-api/spec.js
@@ -1,7 +1,7 @@
 describe('React server side rendering', function () {
     it('should be good', function () {
         browser.get('product?id=124');
-        expect(element.all(by.css('ul li')).count()).toEqual(3);
+        expect(element.all(by.css('h4')).count()).toEqual(1);
     });
 
     it('price should be 12400', function () {
