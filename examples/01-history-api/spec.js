d7ab989862be6a04610588d0901de0a4a3a717d5
Zordius Chen <zordius@yahoo-inc.com>
2014-12-25 17:47:06 +0800

    try to test pjax

--- examples/01-history-api/spec.js
+++ examples/01-history-api/spec.js
@@ -9,3 +9,16 @@ describe('React server side rendering', function () {
         expect(element(by.css('div span')).getInnerHtml()).toMatch(/12400/);
     });
 });
+
+describe('React client side binding', function () {
+    it('should handle button click', function () {
+        browser.get('product?id=124');
+        browser.driver.executeScript('window.test=1');
+        element(by.css('ul li a')).click().then(function () {
+            console.log('OK!');
+            browser.driver.getCurrentUrl().then(function (url) {
+                console.log('URL--------:' + url);
+            });
+        });
+    });
+});
