afc3b9f6941955361e8cd17421708524b9c49a8d
Zordius Chen <zordius@yahoo-inc.com>
2014-12-27 20:39:19 +0800

    fix for ios testing

--- examples/01-history-api/components/Html.jsx
+++ examples/01-history-api/components/Html.jsx
@@ -81,6 +81,7 @@ Html = React.createClass({
         <html>
          <head>
           <meta charSet="utf-8" />
+          <meta name="format-detection" content="telephone=no" />
           <meta name="viewport" content="width=device-width, user-scalable=no" />
           <title>{this.getStore('page').get('title')}</title>
          </head>
