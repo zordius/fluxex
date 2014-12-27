33e970936197370deda5d03b46b5b1ca86ca42a4
Zordius Chen <zordius@yahoo-inc.com>
2014-12-27 23:56:41 +0800

    add client test

--- examples/02-routing/spec.js
+++ examples/02-routing/spec.js
@@ -9,3 +9,20 @@ describe('React server side rendering', function () {
         expect(element(by.css('div h1')).getInnerHtml()).toMatch(/Main/);
     });
 });
+
+describe('React client side binding', function () {
+    it('should handle button click', function () {
+        browser.get('main');
+        browser.driver.executeScript('return window.test=1');
+        element.all(by.css('ul li a')).get(0).click();
+        browser.wait(function () {
+            return browser.driver.getCurrentUrl().then(function (url) {
+                return url.match(/id=123/);
+            });
+        });
+        expect(element(by.css('div span')).getInnerHtml()).toMatch(/12300/);
+        browser.driver.executeScript('return window.test').then(function (value) {
+            expect(value).toBe(1);
+        });
+    });
+});
