f9ee0c0a63e3ecd7d3fb906fb5422088ce997c7f
Zordius Chen <zordius@yahoo-inc.com>
2015-01-01 12:19:48 +0800

    install correct version

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
-    testCommands = ['cd <%=file.path %>;npm install ../..;npm prune;cp -R ..//00hello/node_modules node_modules;npm install;npm run-script disc;npm test'];
+    testCommands = ['cd <%=file.path %>;npm install https://github.com/zordius/fluxex/tarball/' + process.env.ORIGIN_COMMIT + ';npm prune;cp -R ..//00hello/node_modules node_modules;npm install;npm run-script disc;npm test'];
 
 gulp.task('example_tests', function () {
     return gulp.src('*-*/')
