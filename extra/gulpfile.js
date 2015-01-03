6cabb204f978874068a3c59d4b3f55d53cf668b6
Zordius Chen <zordius@yahoo-inc.com>
2015-01-03 10:43:32 +0800

    add jscs

--- extra/gulpfile.js
+++ extra/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    jscs = require('gulp-jscs'),
     jshint = require('gulp-jshint'),
     cached = require('gulp-cached'),
     coverage = require('gulp-jsx-coverage'),
@@ -186,6 +187,7 @@ gulp.task('lint_flux_js', function () {
     return lint_chain(
         gulp.src(build_files.js)
         .pipe(cached('jshint'))
+        .pipe(jscs())
         .pipe(jshint())
     );
 });
@@ -197,6 +199,7 @@ gulp.task('watch_jsx', ['lint_jsx'], function () {
 gulp.task('lint_jsx', function () {
     return lint_chain(
         gulp.src(build_files.jsx)
+        .pipe(jscs())
         .pipe(cached('jshint'))
         .pipe(react_compiler({sourceMap: true}))
         .pipe(jshint(configs.jshint_jsx))
