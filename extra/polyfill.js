26a510f69ff88becc508da2ef7d7f000d2b4042e
Zordius Chen <zordius@yahoo-inc.com>
2015-03-15 16:13:49 +0800

    polyfill solution from babel

---
+++ extra/polyfill.js
@@ -0,0 +1,5 @@
+'use strict';
+// This is a dummy file to enable babel-core polyfill for browser
+// Do this in your first line of fluxex.js to enable polyfill:
+// require('fluxex/extra/polyfill');
+// our gulp task will alias it to 'babelify/polyfill'
