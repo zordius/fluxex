eb9f45ec72461c73f1818e0ce9819295af07fc66
Zordius Chen <zordius@yahoo-inc.com>
2015-03-18 11:03:46 +0800

    add missing file

---
+++ extra/history.js
@@ -0,0 +1,6 @@
+'use strict';
+// This is a dummy file to include history API for client
+// require('fluxex/extra/history') will be replaced with
+// require('html5-history-api')
+// check extra/gulpfile.js to see alias settings
+// Search for aliasify
