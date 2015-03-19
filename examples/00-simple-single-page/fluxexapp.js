219fdb52bfba56203d4e3dc0192ea180bd1e3a3f
Zordius Chen <zordius@yahoo-inc.com>
2015-03-19 21:09:20 +0800

    fix error

--- examples/00-simple-single-page/fluxexapp.js
+++ examples/00-simple-single-page/fluxexapp.js
@@ -6,4 +6,4 @@ module.exports = require('fluxex').createApp({
     sampleStore: require('./stores/sample'),
     page: require('./stores/page'),
     productStore: require('./stores/product')
-}, process.cwd() + '/components/Html.jsx');
+}, require('./components/Html.jsx'));
