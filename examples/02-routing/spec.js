b1440508fb78ed0cc880ed3bc9fb0c8e571a88c4
Zordius Chen <zordius@yahoo-inc.com>
2015-03-20 18:19:59 +0800

    try again. later we may skip safari here.

--- examples/02-routing/spec.js
+++ examples/02-routing/spec.js
@@ -46,9 +46,8 @@ describe('React client side binding', function () {
     });
 
     it('should handle browser back button', function () {
-        // prevent safari issue by javascript history.go(-1)
         // http://stackoverflow.com/questions/27240969/unknown-error-on-safari-history-navigation-using-protractor-webdrivers
-        browser.driver.executeScript('return (window.test=2) + (history.back())');
+        browser.navigate().back();
 
         browser.wait(function () {
             return browser.driver.getCurrentUrl().then(function (url) {
