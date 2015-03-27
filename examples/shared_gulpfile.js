932b70c8dad5b09f63623931cf0e4bdef0e61cd8
Zordius Chen <zordius@yahoo-inc.com>
2015-03-28 00:15:21 +0800

    remove gulp-shell try to fix failed issue

--- examples/shared_gulpfile.js
+++ examples/shared_gulpfile.js
@@ -2,8 +2,22 @@
 // To do Sauce Lab tests
 var gulp = require(process.cwd() + '/node_modules/gulp'),
     nodemon = require(process.cwd() + '/node_modules/nodemon'),
-    shell = require(process.cwd() + '/node_modules/gulp-shell'),
     testcfg = require('./shared_testconf.js');
+    exec = require('child_process').exec,
+
+handleExec = function (cb) {
+    return function (err, stdout, stderr) {
+        console.log(stdout);
+        console.warn(stderr);
+        cb();
+    };
+},
+
+execTask = function (cmd) {
+    return function (cb) {
+        exec(cmd, handleExec(cb));
+    }
+};
 
 gulp.task('test_server', ['buildall'], function () {
     nodemon({
@@ -17,18 +31,19 @@ gulp.task('test_server', ['buildall'], function () {
     });
 });
 
-gulp.task('test_run_protractor', shell.task(
-[
-    'protractor ../shared_testconf.js',
-    'badge-saucelabs-results ' + testcfg.config.job_basename + ' > badge.json',
+gulp.task('test_run_protractor', execTask(
+    'protractor ../shared_testconf.js'
+));
+
+gulp.task('generate_badge_json', ['test_run_protractor'], execTask(
+    'badge-saucelabs-results ' + testcfg.config.job_basename + ' > badge.json'
+));
+
+gulp.task('generate_badge_png', ['generate_badge_json'], execTask(
     'badge-render badge.json badge.html --png badge.png --scale 0.7 -width 490 -height 60'
-],
-{
-    ignoreErrors: true
-}));
+));
 
-
-gulp.task('test_end_protractor', ['test_run_protractor'], function () {
+gulp.task('test_end_protractor', ['generate_badge_json'], function () {
     nodemon.emit('quit');
     process.exit();
 });
