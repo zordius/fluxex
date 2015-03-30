dae6513f9fbfb44b518afe43b8750d684007fd18
Zordius Chen <zordius@yahoo-inc.com>
2015-03-30 20:33:39 +0800

    make gulp tasks simple

--- extra/gulpfile.js
+++ extra/gulpfile.js
@@ -22,10 +22,7 @@ var gulp = require('gulp'),
 // You can :set nowritebackup in vim to prevent this
 // Reference: https://github.com/joyent/node/issues/3172
 configs = {
-    lint_files: {
-        js: ['actions/*.js', 'stores/*.js'],
-        jsx: ['components/*.jsx']
-    },
+    lint_files: ['actions/*.js', 'stores/*.js', 'components/*.jsx', 'fluxexapp.js'],
 
     static_dir: 'static/',
     mainjs: require(process.cwd() + '/package.json').main,
@@ -34,7 +31,6 @@ configs = {
     nodemon_delay: 2000,
     gulp_watch: {debounceDelay: 500},
     watchify: {debug: true, delay: 500},
-    jshint_jsx: {quotmark: false},
     jshint_fail: false,
     jscs_fail: false,
     aliasify: {
@@ -192,47 +188,36 @@ gulp.task('watch_app', function () {
     return buildApp(true, true);
 });
 
-gulp.task('watch_flux_js', ['lint_flux_js'], function () {
-    gulp.watch(configs.lint_files.js, configs.gulp_watch, ['lint_flux_js']);
+gulp.task('watch_js', ['lint_js'], function () {
+    gulp.watch(configs.lint_files.js, configs.gulp_watch, ['lint_js']);
 });
 
-gulp.task('lint_flux_js', function () {
+gulp.task('lint_js', function () {
     return lint_chain(
-        gulp.src(configs.lint_files.js)
+        gulp.src(configs.lint_files)
         .pipe(cached('jshint'))
+        .pipe(babel(Object.assign({sourceMap: true}, configs.babelify)))
         .pipe(jscs()).on('error', handleJSCSError)
         .pipe(jshint())
     );
 });
 
-gulp.task('watch_jsx', ['lint_jsx'], function () {
-    gulp.watch(configs.lint_files.jsx, configs.gulp_watch, ['lint_jsx']);
-});
-
-gulp.task('lint_jsx', function () {
-    return lint_chain(
-        gulp.src(configs.lint_files.jsx)
-        .pipe(cached('jshint'))
-        .pipe(babel(Object.assign({sourceMap: true}, configs.babelify)))
-        .pipe(jscs()).on('error', handleJSCSError)
-        .pipe(jshint(configs.jshint_jsx))
-    );
-});
-
 gulp.task('watch_server', ['lint_server'], function () {
-    gulp.watch(configs.mainjs, configs.gulp_watch, ['lint_server'])
-    .on('change', function () {
-        nodemon.emit('restart');
+    gulp.watch([configs.mainjs, configs.appjs], configs.gulp_watch, ['lint_server'])
+    .on('change', function (E) {
+        if (E.path !== configs.appjs) {
+            nodemon.emit('restart');
+        }
     });
 });
 
 gulp.task('lint_server', function () {
-    return gulp.src(configs.mainjs)
+    return gulp.src([configs.mainjs, configs.appjs])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('nodemon_server', ['watch_flux_js', 'watch_jsx', 'watch_app', 'watch_server'], function () {
+gulp.task('nodemon_server', ['watch_js', 'watch_app', 'watch_server'], function () {
     nodemon({
         ignore: '*',
         script: configs.mainjs,
@@ -269,8 +254,7 @@ gulp.task('nodemon_server', ['watch_flux_js', 'watch_jsx', 'watch_app', 'watch_s
 gulp.task('watch_tests', ['test_app'], function () {
     gulp.watch([
         configs.test_coverage.default.src,
-        configs.lint_files.js,
-        configs.lint_files.jsx
+        configs.lint_files,
     ], ['test_app']);
 });
 gulp.task('test_app', function (cb) {
@@ -288,7 +272,7 @@ gulp.task('save_test_app', function () {
     return get_testing_task(configs.test_coverage.report)();
 });
 gulp.task('develop', ['nodemon_server']);
-gulp.task('lint_all', ['lint_server', 'lint_flux_js', 'lint_jsx']);
+gulp.task('lint_all', ['lint_server', 'lint_js']);
 gulp.task('buildall', ['lint_all', 'build_app']);
 gulp.task('default',['buildall']);
 
