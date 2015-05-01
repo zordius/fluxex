b321f7848a1d2e5994530b1ab4d822231b7aae1e
Zordius Chen <zordius@yahoo-inc.com>
2015-05-01 23:54:15 +0800

    add rpc-seturl.js

---
+++ extra/rpc-seturl.js
@@ -0,0 +1,5 @@
+// For Fluxex Applications
+// Add this line in your fluxexapp.js when you need to change rpc baseURL:
+// require('fluxex/extra/rpc-seturl')('/your_new_base_url/');
+// check example here: https://github.com/zordius/fluxex/blob/master/examples/05-extra/fluxexapp.js
+module.exports = require('iso-call').setBaseURL;
