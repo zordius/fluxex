5633d5e0235765b538cd5714424a850818fe1cec
Zordius Chen <zordius@yahoo-inc.com>
2015-05-20 18:42:46 +0800

    adopt console-polyfill

--- extra/polyfill-ie8-client.js
+++ extra/polyfill-ie8-client.js
@@ -8,6 +8,24 @@ if (XMLHttpRequest && !XMLHttpRequest.OPENED) {
     XMLHttpRequest.DONE = 4;
 }
 
+// Console-polyfill. MIT license.
+// https://github.com/paulmillr/console-polyfill
+// Make it safe to do console.log() always.
+(function(global) {
+  'use strict';
+  global.console = global.console || {};
+  var con = global.console;
+  var prop, method;
+  var empty = {};
+  var dummy = function() {};
+  var properties = 'memory'.split(',');
+  var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
+     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
+     'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
+  while (prop = properties.pop()) if (!con[prop]) con[prop] = empty;
+  while (method = methods.pop()) if (!con[method]) con[method] = dummy;
+})(typeof window === 'undefined' ? this : window);
+
 // From https://github.com/jonathantneal/EventListener
 // CC0-1.0 License
 // EventListener | CC0 | github.com/jonathantneal/EventListener
