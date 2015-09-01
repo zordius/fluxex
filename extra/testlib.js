4c58e9f5a4f425d34925410c2a6462fba859fa77
Zordius Chen <zordius@yahoo-inc.com>
2015-09-01 15:32:40 +0800

    add testlib

---
+++ extra/testlib.js
@@ -0,0 +1,91 @@
+var jsdom = require('jsdom').jsdom;
+var sinon = require('sinon');
+var React = require('react');
+var fluxex = require('..');
+var fluxexhtml = require('../lib/fluxhtml');
+var fluxstore = require('../lib/fluxstore');
+
+var Testlib = {
+    getReactTestUtils: function () {
+         var nodeenv = process.env.NODE_ENV;
+         var TestUtils;
+
+         process.env.NODE_ENV = 'testing';
+         TestUtils = require('react/addons').addons.TestUtils;
+         process.env.NODE_ENV = nodeenv;
+
+         return TestUtils;
+    },
+
+    simulate: function () {
+         return Testlib.getReactTestUtils().Simulate;
+    },
+
+    getMockedContext: function (mockStores) {
+        var context = new fluxex();
+        var Stores = [];
+        var Store;
+        var I;
+
+        sinon.stub(context, 'dispatch').returns(Promise.resolve(1));
+
+        Object.assign(context, require('fluxex/extra/rpc'));
+        // should be removed after all fetch migrated to rpc
+        Object.assign(context, require('fluxex/extra/fetch').appMixin);
+
+        if (mockStores) {
+            sinon.stub(context, 'getStore', function (store) {
+                if(Stores[store]){
+                    return Stores[store]; 
+                } else {
+                    Store = new fluxstore();
+
+                    sinon.stub(Store, '_get', function (name) {
+                        return mockStores[store][name];
+                    });
+
+                    Stores[store] = Store;
+
+                    return Store;
+                }
+            });
+        }
+ 
+        return context;
+    },
+
+    getContextedElement: function (react, props, context) {
+        return React.createFactory(fluxexhtml)({
+            fluxex: context || Testlib.getMockedContext(),
+            html: function () {
+                return React.createFactory(react)(Object.assign({}, props));
+            }
+        });
+    },
+
+    renderJSX: function (jsx, context) {
+        return Testlib.renderComponent(React.createClass({
+            displayName: 'TestJSX',
+            render: function () {return jsx;}
+        }), undefined, context);
+    },
+
+    simulateBrowserEnv: function () {
+        global.document = jsdom('<!DOCTYPE html><html><body></body></html>');
+        global.window = document.parentWindow;
+    },
+
+    stopBrowserEnv: function () {
+        delete global.window;
+        delete global.document;
+        require('react/lib/ExecutionEnvironment').canUseDOM = false;
+    },
+
+    renderComponent: function (react, props, context) {
+        var rendered;
+
+        Testlib.simulateBrowserEnv();
+        rendered = Testlib.getReactTestUtils().renderIntoDocument(Testlib.getContextedElement(react, props, context));
+        return Testlib.getReactTestUtils().findRenderedComponentWithType(rendered, react);
+    }
+}
