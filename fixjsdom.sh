db8510f68fa8a7c7aa059cca84e71dad7c21aa35
Zordius Chen <zordius@yahoo-inc.com>
2016-01-16 18:31:07 +0800

    try to fix for iojs

--- fixjsdom.sh
+++ fixjsdom.sh
@@ -9,4 +9,9 @@ if [ "${TRAVIS_BUILD_NUMBER}.2" = "${TRAVIS_JOB_NUMBER}" ]; then
   exit 0
 fi
 
+if [ "${TRAVIS_BUILD_NUMBER}.3" = "${TRAVIS_JOB_NUMBER}" ]; then
+  npm install jsdom@6.5.1
+  exit 0
+fi
+
 npm install jsdom@7.2.2 
