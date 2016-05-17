4d184660d0878b9e084233d97e2ccdb3909b65e3
Zordius Chen <zordius@yahoo-inc.com>
2016-05-17 17:55:57 +0800

    refine nodejs and jsdom testing versions

--- fixjsdom.sh
+++ fixjsdom.sh
@@ -5,13 +5,8 @@ if [ "${TRAVIS_BUILD_NUMBER}.1" = "${TRAVIS_JOB_NUMBER}" ]; then
 fi
 
 if [ "${TRAVIS_BUILD_NUMBER}.2" = "${TRAVIS_JOB_NUMBER}" ]; then
-  npm install jsdom@3.1.2
-  exit 0
-fi
-
-if [ "${TRAVIS_BUILD_NUMBER}.3" = "${TRAVIS_JOB_NUMBER}" ]; then
   npm install jsdom@6.5.1
   exit 0
 fi
 
-npm install jsdom@7.2.2 
+npm install jsdom@9.1.0
