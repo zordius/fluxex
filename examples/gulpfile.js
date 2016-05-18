589c4214414057f59795fc2edcadc70dd81cf16c
Zordius Chen <zordius@yahoo-inc.com>
2016-05-18 16:31:51 +0800

    use shell script to test

--- examples/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var exec = require('gulp-exec');
-var testCommands = 'cd <%=file.path %>;npm prune;npm install;npm install https://github.com/zordius/fluxex/tarball/' + process.env.ORIGIN_COMMIT + ';npm run-script disc;npm test';
-
-gulp.task('example_tests', function () {
-    return gulp.src('*-*/')
-    .pipe(exec(testCommands))
-    .pipe(exec.reporter());
-});
